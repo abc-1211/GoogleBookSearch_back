98c58430bf8e8ebd4835260fb851771168e2a06b
ian <chengkwanyu@yahoo.com.hk>
2020-10-05 17:35:02 +0800

    server created

---
+++ server.js
@@ -0,0 +1,37 @@
+// NPM
+const express = require('express');
+const cors = require("cors");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Route
+const router = require('./routes/index');
+
+// Configure dotenv
+dotenv.config();
+
+// Define PORT
+const PORT = process.env.PORT || 8080;
+
+// Define express server
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+// Specify API routes
+app.use('/api', router);
+
+// Conect to MongoDB using mongoose
+mongoose.connect('mongodb://localhost/googleBooksDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('successfully connected to database');
+});
+mongoose.set('useCreateIndex', true)
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
